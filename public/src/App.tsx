70c7ea03442f3119f2e9e585d8c110874483057b
Thhoorr <thorbaker11@gmail.com>
2026-05-12 16:24:51 -0700

    changed the project structure, so that frontend files are in public and backend files in the root.

---
+++ public/src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage.js";
+import MapPage from "./pages/MapPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/map" element={<MapPage />} />
+    </Routes>
+  );
+}
+
+export default App;
